7ed6d70f0685df2771f05f5c0cb314f81e6c44ba
Mohammed Anish B <mohammed.ab@travancoreanalytics.com>
2025-03-28 19:01:00 +0530

    Added firebase realtime database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:ablecredit/firebase_options.dart';
 import 'package:ablecredit/view/home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
